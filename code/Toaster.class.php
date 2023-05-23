e262134de627c0f45adea6deb512b946aa82df05
jefferson <jefferson.ferraz@live.com>
2023-05-23 11:27:02 +0200

    update

--- code/Toaster.class.php
+++ code/Toaster.class.php
@@ -8,12 +8,69 @@ class Toaster {
     public $toastZustand;
     public $zeit;
 
-    public function toastReintun () {}
+    public function __construct($farbe, $schaechte, $anzahlToasts, $zeit) {
 
-    public function toastAuswerfen () {}
+        $this->farbe = $farbe;
+        $this->schaechte = $schaechte;
+        $this->anzahlToasts = $anzahlToasts;
+        $this->zeit = $zeit;
+        
+    }
 
-    public function toasten () {}
+    public function toastReintun() {
 
-    public function toastZeiteinstellen () {}
+        if ($this->anzahlToasts <= $this->schaechte) {
+
+            echo "Passt!";
+
+        } else {
+
+            echo "Passt nicht!";
+
+        }
+
+    }
+
+    public function toasten() {
+
+        if ($this->zeit > 0) {
+
+            $this->toastZeiteinstellen();
+
+        } else {
+
+            echo "Kein Toast!";
+
+        }
+
+    }
+
+    public function toastZeiteinstellen() {
+
+        if ($this->zeit > 0 && $this->zeit <= 15) {
+
+            $this->toastZustand = "leicht getoastet!";
+
+        } elseif ($this->zeit > 15 && $this->zeit <= 30) {
+
+            $this->toastZustand = "stark getoastet!";
+
+        } elseif ($this->zeit > 30) {
+
+            $this->toastZustand = "verbrannt!";
+
+        } else {
+
+            $this->toastZustand = "ungetoastet!";
+
+        }
+
+    }
+
+    public function toastAuswerfen() {
+
+       echo "Der Toast ist {$this->toastZustand}";
+
+    }
 
 }
