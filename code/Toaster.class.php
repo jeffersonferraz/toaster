d9b7a83da63b12ebe9a77ca1d1bc660c7ae7a8c4
jefferson <jefferson.ferraz@live.com>
2023-05-23 10:32:47 +0200

    update

---
+++ code/Toaster.class.php
@@ -0,0 +1,19 @@
+<?php
+
+class Toaster {
+
+    public $farbe;
+    public $schaechte;
+    public $anzahlToasts;
+    public $toastZustand;
+    public $zeit;
+
+    public function toastReintun () {}
+
+    public function toastAuswerfen () {}
+
+    public function toasten () {}
+
+    public function toastZeiteinstellen () {}
+
+}
